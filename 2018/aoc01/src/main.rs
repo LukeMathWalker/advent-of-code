954b16bc04e50a94e627a4c8a61a1c0ae2c91b3a
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-12-03 21:56:24 +0000

    Restructured to use for both part 1 and 2

--- 2018/aoc01/src/main.rs
+++ 2018/aoc01/src/main.rs
@@ -3,7 +3,13 @@ use std::str::FromStr;
 use std::fs::File;
 
 fn main() -> std::io::Result<()> {
-    let f = File::open("input/part1.txt")?;
+    let input_fp = "input/part1.txt";
+    part1(&input_fp)?;
+    Ok(())
+}
+
+fn part1(input_fp: &str) -> std::io::Result<()> {
+    let f = File::open(input_fp)?;
     let reader = BufReader::new(f);
     let sum: isize = reader
         .lines()
