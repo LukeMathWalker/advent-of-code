a74056e0b47a809f4dea45ded36ad6ff111c1a64
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-12-03 21:51:54 +0000

    Day 01 - Part 1

--- 2018/aoc01/src/main.rs
+++ 2018/aoc01/src/main.rs
@@ -5,10 +5,12 @@ use std::fs::File;
 fn main() -> std::io::Result<()> {
     let f = File::open("input/part1.txt")?;
     let reader = BufReader::new(f);
-    let frequencies = reader
+    let sum: isize = reader
         .lines()
         .map(|l| l.expect("Failed to read line."))
-        .map(|l| isize::from_str(&l).expect("Failed to convert to integer."));
+        .map(|l| isize::from_str(&l).expect("Failed to convert to integer."))
+        .sum();
 
+    println!("Calibrated frequency: {:?}.", sum);
     Ok(())
 }
