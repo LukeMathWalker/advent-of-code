fa361cd0088ef64b67956002dca62c8814c800bb
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-12-03 22:04:42 +0000

    Refactored loading

--- 2018/aoc01/src/main.rs
+++ 2018/aoc01/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{BufReader, BufRead};
 use std::str::FromStr;
 use std::fs::File;
+use std::iter::Iterator;
 
 fn main() -> std::io::Result<()> {
     let input_fp = "input/part1.txt";
@@ -8,14 +9,19 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn part1(input_fp: &str) -> std::io::Result<()> {
+fn read_input(input_fp: &str) -> std::io::Result<impl Iterator<Item=isize>> {
     let f = File::open(input_fp)?;
     let reader = BufReader::new(f);
-    let sum: isize = reader
+    let frequencies = reader
         .lines()
         .map(|l| l.expect("Failed to read line."))
-        .map(|l| isize::from_str(&l).expect("Failed to convert to integer."))
-        .sum();
+        .map(|l| isize::from_str(&l).expect("Failed to convert to integer."));
+    Ok(frequencies)
+}
+
+fn part1(input_fp: &str) -> std::io::Result<()> {
+    let frequencies = read_input(input_fp)?;
+    let sum: isize = frequencies.sum();
 
     println!("Calibrated frequency: {:?}.", sum);
     Ok(())
