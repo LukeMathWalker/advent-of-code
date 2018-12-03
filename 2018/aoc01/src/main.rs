321f898dbe5835849443a63bfa93df2c496523e1
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-12-03 21:40:14 +0000

    Load frequencies

--- 2018/aoc01/src/main.rs
+++ 2018/aoc01/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{BufReader, BufRead};
+use std::str::FromStr;
+use std::fs::File;
+
+fn main() -> std::io::Result<()> {
+    let f = File::open("../input/part01.txt")?;
+    let reader = BufReader::new(f);
+    let frequencies = reader
+        .lines()
+        .map(|l| l.expect("Failed to read line."))
+        .map(|l| isize::from_str(&l).expect("Failed to convert to integer."));
+
+    Ok(())
 }
