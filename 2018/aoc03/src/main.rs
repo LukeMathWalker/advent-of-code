355423b6e0cd0b0ae17cbba98dda4d1872395df7
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-12-04 09:22:18 +0000

    Parsed the input

--- 2018/aoc03/src/main.rs
+++ 2018/aoc03/src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(try_trait)]
+extern crate lazy_static;
+extern crate regex;
+use lazy_static::lazy_static;
+use regex::{Captures, Regex};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::str::FromStr;
+
+fn main() -> std::io::Result<()> {
+    let input_fp = "input/part1.txt";
+    let side_length = 1000;
+    part1(&input_fp, side_length)?;
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct Rectangle {
+    pub min_x: usize,
+    pub max_x: usize,
+    pub min_y: usize,
+    pub max_y: usize,
+}
+
+impl Rectangle {
+    pub fn from_str(s: &str, fabric_side_length: usize) -> Result<Self, std::option::NoneError> {
+        let caps = Rectangle::parse(s).unwrap();
+        let min_x = usize::from_str(caps.get(1)?.as_str()).unwrap();
+        let max_y = fabric_side_length - usize::from_str(caps.get(2)?.as_str()).unwrap();
+        let max_x = min_x + usize::from_str(caps.get(3)?.as_str()).unwrap();
+        let min_y = max_y - usize::from_str(caps.get(4)?.as_str()).unwrap();
+        Ok(Rectangle {
+            min_x,
+            max_x,
+            min_y,
+            max_y,
+        })
+    }
+
+    fn parse(s: &str) -> Option<Captures> {
+        lazy_static! {
+            static ref RE: Regex =
+                Regex::new(r"#\d{1,4} @ (\d{1,4}),(\d{1,4}): (\d{1,4})x(\d{1,4})").unwrap();
+        }
+        RE.captures(s)
+    }
+}
+
+fn read_input(input_fp: &str) -> std::io::Result<impl Iterator<Item = String>> {
+    let f = File::open(input_fp)?;
+    let reader = BufReader::new(f);
+    let lines = reader.lines().map(|l| l.expect("Failed to read line."));
+    Ok(lines)
+}
+
+fn part1(input_fp: &str, fabric_side_length: usize) -> std::io::Result<()> {
+    let lines = read_input(input_fp)?;
+    let rectangles: Vec<Rectangle> = lines
+        .map(|l| Rectangle::from_str(&l, fabric_side_length).unwrap())
+        .collect();
+    println!("{:?}", rectangles);
+    Ok(())
 }
