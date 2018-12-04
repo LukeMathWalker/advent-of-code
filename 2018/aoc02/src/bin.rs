37b0d03b0d10e8f71f9ed6382c50cd1a0891e66c
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-12-04 08:20:54 +0000

    Refactored part1

--- 2018/aoc02/src/bin.rs
+++ 2018/aoc02/src/bin.rs
@@ -1,3 +1,5 @@
+extern crate lib;
+use lib::{BoxId, Checker};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -7,48 +9,23 @@ use std::string::String;
 fn main() -> std::io::Result<()> {
     let input_fp = "input/part1.txt";
     part1(&input_fp)?;
-    part2(&input_fp)?;
+//    part2(&input_fp)?;
     Ok(())
 }
 
-fn read_input(input_fp: &str) -> std::io::Result<impl Iterator<Item = String>> {
+fn read_input(input_fp: &str) -> std::io::Result<impl Iterator<Item = BoxId>> {
     let f = File::open(input_fp)?;
     let reader = BufReader::new(f);
-    let box_ids = reader.lines().map(|l| l.expect("Failed to read line."));
+    let box_ids = reader
+        .lines()
+        .map(|l| l.expect("Failed to read line."))
+        .map(|s| BoxId::from(s));
     Ok(box_ids)
 }
 
-fn char_counter(s: &str) -> HashMap<char, usize> {
-    let mut counter = HashMap::new();
-    for c in s.chars() {
-        let count = counter.entry(c).or_insert(0);
-        *count += 1;
-    }
-    counter
-}
-
-fn has_any_char_appearing_k_times(k: usize, c: &HashMap<char, usize>) -> bool {
-    for count in c.values() {
-        if count == &k {
-            return true;
-        }
-    }
-    false
-}
-
 fn part1(input_fp: &str) -> std::io::Result<()> {
-    let box_ids = read_input(input_fp)?;
-    let char_counters = box_ids.map(|id| char_counter(&id));
-    let checks: (usize, usize) = char_counters
-        .map(|counter| {
-            (
-                has_any_char_appearing_k_times(2, &counter) as usize,
-                has_any_char_appearing_k_times(3, &counter) as usize,
-            )
-        }).fold((0, 0), |sums, values| {
-            (sums.0 + values.0, sums.1 + values.1)
-        });
-    let checksum = checks.0 * checks.1;
+    let box_ids: Vec<BoxId> = read_input(input_fp)?.collect();
+    let checksum = box_ids.as_slice().check_sum();
     println!("Checksum: {:}", checksum);
 
     Ok(())
@@ -89,13 +66,13 @@ fn common_chars(s: &str, t: &str) -> Vec<char> {
     common
 }
 
-fn part2(input_fp: &str) -> std::io::Result<()> {
-    let box_ids: Vec<String> = read_input(input_fp)?.collect();
-    let box_ids_with_suit = find_boxes_with_suit(&box_ids).expect("No ids are close.");
-    println!("Box ids with suit: {:?}", box_ids_with_suit);
-    let common_letters: String = common_chars(box_ids_with_suit.0, box_ids_with_suit.1)
-        .into_iter()
-        .collect();
-    println!("Letter in common: {:?}", common_letters);
-    Ok(())
-}
+//fn part2(input_fp: &str) -> std::io::Result<()> {
+//    let box_ids: Vec<String> = read_input(input_fp)?.collect();
+//    let box_ids_with_suit = find_boxes_with_suit(&box_ids).expect("No ids are close.");
+//    println!("Box ids with suit: {:?}", box_ids_with_suit);
+//    let common_letters: String = common_chars(box_ids_with_suit.0, box_ids_with_suit.1)
+//        .into_iter()
+//        .collect();
+//    println!("Letter in common: {:?}", common_letters);
+//    Ok(())
+//}
--- 2018/aoc02/src/lib.rs
+++ 2018/aoc02/src/lib.rs
@@ -1,9 +1,56 @@
+use std::collections::HashMap;
+
 pub struct BoxId {
     id: String,
 }
+
+type CharCounter = HashMap<char, usize>;
 
 impl From<String> for BoxId {
     fn from(s: String) -> Self {
         BoxId {id: s}
     }
 }
+
+impl BoxId {
+    pub fn count(&self) -> CharCounter {
+        let mut counter = CharCounter::new();
+        for c in self.id.chars() {
+            let count = counter.entry(c).or_insert(0);
+            *count += 1;
+        }
+        counter
+    }
+}
+
+pub trait Checker {
+    fn check_sum(&self) -> usize;
+}
+
+impl<'a> Checker for &'a [BoxId] {
+    fn check_sum(&self) -> usize {
+        let checks: (usize, usize) = self
+            .iter()
+            .map(|id| id.count())
+            .map(|counter| {
+                (
+                    has_any_char_appearing_k_times(2, &counter) as usize,
+                    has_any_char_appearing_k_times(3, &counter) as usize,
+                )
+            }).fold((0, 0), |sums, values| {
+            (sums.0 + values.0, sums.1 + values.1)
+        });
+        let checksum = checks.0 * checks.1;
+        checksum
+    }
+}
+
+fn has_any_char_appearing_k_times(k: usize, c: &CharCounter) -> bool {
+    for count in c.values() {
+        if count == &k {
+            return true;
+        }
+    }
+    false
+}
+
