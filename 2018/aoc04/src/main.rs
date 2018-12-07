e81dbd70e32928f0044717a306ec99e7f4d467c0
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-12-07 16:12:45 +0000

    Read input

--- 2018/aoc04/src/main.rs
+++ 2018/aoc04/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+fn main() -> std::io::Result<()> {
+    let input_fp = "../input/part01.txt";
     println!("Hello, world!");
+    let input = read_input(input_fp)?;
+    Ok(())
+}
+
+fn read_input(input_fp: &str) -> std::io::Result<impl Iterator<Item=String>> {
+    let f = File::open(input_fp)?;
+    let reader = BufReader::new(f);
+    let lines = reader.lines().map(|l| l.expect("Failed to read line."));
+    Ok(lines)
 }
