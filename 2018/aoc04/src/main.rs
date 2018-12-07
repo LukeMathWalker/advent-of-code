f4dca73c71bbb2ec8a253ef7f98139f039889a16
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-12-07 16:59:17 +0000

    Parsed input successfully

--- 2018/aoc04/src/main.rs
+++ 2018/aoc04/src/main.rs
@@ -1,6 +1,7 @@
 use chrono::NaiveDateTime;
 use lazy_static::lazy_static;
 use regex::{Captures, Regex};
+use std::str::FromStr;
 use std::fs::File;
 use std::string::ParseError;
 use std::io::{BufRead, BufReader};
@@ -11,7 +12,6 @@ fn main() -> std::io::Result<()> {
     let parsed_input = input.iter().map(|s| parse(s));
     for s in parsed_input {
         println!("{:?}", s);
-        break;
     }
     Ok(())
 }
@@ -28,13 +28,41 @@ fn parse_input(input: &[&str]) {
     unimplemented!()
 }
 
-fn parse(s: &str) -> NaiveDateTime {
+#[derive(Debug)]
+struct Event {
+    timestamp: NaiveDateTime,
+    action: Action,
+    guard_id: Option<usize>,
+}
+
+#[derive(Debug)]
+enum Action {
+    BeginShift,
+    FallsAsleep,
+    WakesUp,
+}
+
+fn parse(s: &str) -> Event {
     lazy_static! {
-        static ref RE: Regex = Regex::new(r"\[(?P<time>.{16})\]\s(?P<action>.*)").unwrap();
+        static ref RE_input: Regex = Regex::new(r"\[(?P<time>.{16})\]\s(?P<action>.*)").unwrap();
     }
-    let captures = RE.captures(s).expect("Failed to parse input string.");
+    lazy_static! {
+        static ref RE_action: Regex = Regex::new(r"Guard #(?P<id>\d+) begins shift").unwrap();
+    }
+    let captures = RE_input.captures(s).expect("Failed to parse input string.");
+
     let time = captures.name("time").expect("Failed to extract datetime").as_str();
-    let action = captures.name("action").expect("Failed to extract action").as_str();
     let time = NaiveDateTime::parse_from_str(time, "%Y-%m-%d %H:%M").expect("Failed to convert string representation to datetime format.");
-    time
+
+    let action = captures.name("action").expect("Failed to extract action").as_str();
+    let (action, guard_id) = match action {
+        "wakes up" => (Action::WakesUp, None),
+        "falls asleep" => (Action::FallsAsleep, None),
+        s => {
+            let captures = RE_action.captures(s).expect("Failed to parse action.");
+            let guard_id = captures.name("id").expect("Failed to extract guard id.").as_str();
+            (Action::BeginShift, Some(usize::from_str(guard_id).unwrap()))
+        }
+    };
+    Event { timestamp: time, action, guard_id }
 }
