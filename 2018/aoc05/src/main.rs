bbabc4be9996f940978654740e5b33f221943951
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-12-17 08:27:54 +0000

    Finished day 5

--- 2018/aoc05/src/main.rs
+++ 2018/aoc05/src/main.rs
@@ -1,8 +1,12 @@
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::Read;
 use std::iter::FromIterator;
 
-fn read_input(input_fp: &str) -> std::io::Result<String> {
+type Molecule = char;
+type Polymer = String;
+
+fn read_input(input_fp: &str) -> std::io::Result<Polymer> {
     let mut f = File::open(input_fp)?;
     let mut input = String::new();
     f.read_to_string(&mut input)?;
@@ -13,20 +17,38 @@ fn read_input(input_fp: &str) -> std::io::Result<String> {
 fn main() -> std::io::Result<()> {
     let input_fp = "input/part1.txt";
     let polymer = read_input(input_fp)?;
-    let mut old_polymer = polymer;
-    loop {
-        let new_polymer = reduce(&old_polymer);
-        if new_polymer == old_polymer {
-            break;
-        } else {
-            old_polymer = new_polymer;
-        }
+    let reduced_polymer = reduce(&polymer);
+    println!("The reduced polymer has {:} units.", reduced_polymer.len());
+    let molecules: HashSet<Molecule> = polymer.to_ascii_lowercase().chars().collect();
+    let mut removal_gains: HashMap<Molecule, usize> = HashMap::new();
+    for molecule in molecules {
+        let l: Polymer = polymer
+            .chars()
+            .filter(|m| m.to_ascii_lowercase() != molecule)
+            .collect();
+        removal_gains.insert(molecule, reduce(&l).len());
     }
-    println!("The reduce polymer has {:} units.", old_polymer.len());
+    println!(
+        "The shortest length we can achieve is {:?}.",
+        removal_gains.values().min().unwrap()
+    );
     Ok(())
 }
 
-fn reduce(p: &str) -> String {
+fn reduce(p: &Polymer) -> Polymer {
+    let mut old = p.to_string();
+    loop {
+        let new = _reduce(&old);
+        if new == old {
+            break;
+        } else {
+            old = new;
+        }
+    }
+    old
+}
+
+fn _reduce(p: &Polymer) -> Polymer {
     let mut iter = p.chars().peekable();
     let mut reduced = vec![];
     loop {
@@ -52,10 +74,10 @@ fn reduce(p: &str) -> String {
             }
         }
     }
-    String::from_iter(reduced)
+    Polymer::from_iter(reduced)
 }
 
-fn are_compatible(r: &char, s: &char) -> bool {
+fn are_compatible(r: &Molecule, s: &Molecule) -> bool {
     if r.to_ascii_lowercase() == s.to_ascii_lowercase() {
         if r.is_ascii_lowercase() == s.is_ascii_uppercase() {
             true
